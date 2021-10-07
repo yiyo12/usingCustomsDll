6105e50779b793eb6a07bcf833aef17c151ce305
Carlos Coronado <yiyoskyrim@live.com>
2021-10-07 08:17:58 -0700

    custom math dll

---
+++ MyCustomArithmeticLibrary/calculate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyCustomArithmeticLibrary
+{
+    public class calculate
+    {
+        public int sum(int a, int b) {
+            return a + b;
+        }
+
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int mul(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ UseCustomsDll/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MyCustomArithmeticLibrary;
+namespace UseCustomsDll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyCustomArithmeticLibrary.calculate obj= new MyCustomArithmeticLibrary.calculate();
+            var resultSuma = obj.sum(3, 5);
+            Console.WriteLine(resultSuma);
+        }
+    }
+}
